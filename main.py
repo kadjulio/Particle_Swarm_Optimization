d523097328da99ae56262fa5b1c4af4596c42674
Tgey <thomas.gey@outlook.fr>
2019-10-31 00:25:44 +0000

    feedforward and neurons archi improvement

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import model as ml
-import functions.activitions as activations
+import functions.activitions as act
 
-ann = ml.MultiANN(1, 2, 2, [2, 2, 2])
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+feature_set, labels = datasets.make_moons(3, noise=0.10)
+Y = np.random.randn(len(feature_set[0]))
+nb_layers = 2
+layers_dim = [len(feature_set[0]), 4, 1] #first value corresponds to input dimension
+activation_functions = [act.Sigmoid, act.Sigmoid]
+
+ann = ml.MultiANN(feature_set, Y, nb_layers, layers_dim, activation_functions)
+
 ann.layers_init()
-ann.forward([activations.Relu, activations.Sigmoid])
+ann.train(epochs=2)
--- model/multiANN.py
+++ model/multiANN.py
@@ -1,30 +1,37 @@
 import numpy as np
 
 class MultiANN:
-    def __init__(self, X, Y, nb_layers, layers_dim):
+    def __init__(self, X, Y, nb_layers, layers_dim, activation_functions):
         self.X = X
         self.Y = Y
         self.nblayers = nb_layers
         self.dims = layers_dim
         self.layers = {}
+        self.act_fcts = activation_functions
         
     def layers_init(self):    
-        np.random.seed(1)
+        np.random.seed(42)
         for i in range(1, self.nblayers + 1, 1):
-            self.layers['W' + str(i)] = np.random.randn(self.dims[i], self.dims[i - 1]) / np.sqrt(self.dims[i -1]) 
-            self.layers['b' + str(i)] = np.zeros((self.dims[i], 1))                       
+            self.layers['W' + str(i)] = np.random.randn(self.dims[i - 1], self.dims[i]) / np.sqrt(self.dims[i]) 
 
         for x in self.layers:
             print (x, ':', self.layers[x])            
         return
 
-    def forward(self, activation_functions):    
-        Z1 = self.layers['W1'].dot(self.X) + self.layers['b1'] 
-        A1 = activation_functions[0](Z1)
+    def forward(self):    
+        for raw in self.X:
+            Z = np.dot(raw, self.layers['W1'])
+            act_val = self.act_fcts[0](Z)
+            print("\n\nlayers n°1: " + str(act_val))
 
-        for i, act in zip(range(2, self.nblayers + 1, 1), activation_functions[1:]):
-            Z2 = self.layers['W' + str(i)].dot(A1) + self.layers['b' + str(i)]  
-            A2 = act(Z2)
+            for i, act in zip(range(2, self.nblayers + 1, 1), self.act_fcts[1:]):
+                Z = np.dot(act_val, self.layers['W' + str(i)]) 
+                act_val = act(Z)
+                print("layers n°" + str(i) + ": " + str(act_val))
         
-        self.Yh=A2
-        return self.Yh
+        self.Yo=act_val
+        return self.Yo
+
+    def train(self, epochs):
+        for epoch in range(epochs):
+            self.forward()
